08b0b7aa3d3846aa3cc188aa5acfdcffd99d9827
unknown <sebastien.rozange@MW773VKMUETGY6.dir.svc.accenture.com>
2015-02-18 09:48:26 +0100

    replace '+' by '^' in search filters

--- src/main/java/org/betarss/core/FeedFilter.java
+++ src/main/java/org/betarss/core/FeedFilter.java
@@ -14,22 +14,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FeedFilter {
-	
+
 	private static final Pattern expressionPattern = Pattern.compile("[\\!\\d\\w_\\- ]+", Pattern.CASE_INSENSITIVE);
-	
+
 	@SuppressWarnings("unchecked")
 	public Feed filter(Feed feed, String filter) throws FeedFilterException {
 		Feed result = null;
 		try {
 			Query q = new Query();
-			q.parse ("SELECT * FROM org.betarss.domain.FeedItem WHERE " + getAsSql(filter));
-			QueryResults qr = q.execute (feed.getFeedItems());
+			q.parse("SELECT * FROM org.betarss.domain.FeedItem WHERE " + getAsSql(filter));
+			QueryResults qr = q.execute(feed.getFeedItems());
 			List<FeedItem> res = qr.getResults();
 			result = FeedBuilder.start().withTitle(feed.getTitle()).withFeedItems(res).get();
 
 		} catch (org.josql.QueryParseException e) {
 			throw new FeedFilterException(e);
-			
+
 		} catch (QueryExecutionException e) {
 			throw new FeedFilterException(e);
 		}
@@ -41,14 +41,14 @@ public class FeedFilter {
 		Matcher matcher = expressionPattern.matcher(filter);
 		while (matcher.find()) {
 			String expr = matcher.group(0);
-			if (expr.startsWith("!")) {	
+			if (expr.startsWith("!")) {
 				matcher.appendReplacement(expression, " title not LIKE '%" + expr.substring(1) + "%' ");
 			} else {
 				matcher.appendReplacement(expression, " title LIKE '%" + expr + "%' ");
 			}
 		}
 		matcher.appendTail(expression);
-		return expression.toString().replaceAll("\\|\\|", " OR ").replaceAll("\\+", " AND ");
+		return expression.toString().replaceAll("\\|\\|", " OR ").replaceAll("\\^", " AND ");
 	}
-	
+
 }
