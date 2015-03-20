90703c5ed506f6796e34074cbfa93ef1fafe11ca
unknown <sebastien.rozange@MW773VKMUETGY6.dir.svc.accenture.com>
2015-03-20 14:02:04 +0100

    minor

--- src/connected/test/org/betarss/resource/BetarssRessourceConnectedTest.java
+++ src/connected/test/org/betarss/resource/BetarssRessourceConnectedTest.java
@@ -25,10 +25,10 @@ public class BetarssRessourceConnectedTest {
 	@Test
 	public void search_matches_with_VF_language() throws Exception {
 		HttpEntity<byte[]> feed = resource.feed("game of thrones", 4, "fr", null, null, null, true, true);
-		Assertions.assertThat(asString(feed)).isEqualTo(expected("VF_language"));
+		Assertions.assertThat(asString(feed)).isEqualTo(expectedResult("VF_language"));
 	}
 
-	private String expected(String testId) throws IOException {
+	private String expectedResult(String testId) throws IOException {
 		Resource resource = new ClassPathResource("search_results_for_" + testId + ".xml");
 		return Files.toString(resource.getFile(), Charsets.UTF_8);
 	}
