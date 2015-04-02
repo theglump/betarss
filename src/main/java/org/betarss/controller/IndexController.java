cb8c52f906b1d4678457c30a868be940187226e5
unknown <sebastien.rozange@MW773VKMUETGY6.dir.svc.accenture.com>
2015-04-02 11:46:38 +0200

    coco

--- src/main/java/org/betarss/controller/IndexController.java
+++ src/main/java/org/betarss/controller/IndexController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/")
 @Controller
+@RequestMapping("/")
 public class IndexController {
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String index() {
+		return "index.html";
+	}
+
+	@RequestMapping(value = "api", method = RequestMethod.GET)
+	public String ind() {
 		return "index.html";
 	}
 }
