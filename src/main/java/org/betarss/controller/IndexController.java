fa0ba912d5bd468321da04c1cd84b9b435062634
unknown <sebastien.rozange@MW773VKMUETGY6.dir.svc.accenture.com>
2015-04-02 11:29:56 +0200

    a

---
+++ src/main/java/org/betarss/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.betarss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/")
+@Controller
+public class IndexController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String index() {
+		return "index.html";
+	}
+}
