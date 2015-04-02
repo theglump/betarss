f9d990880d9c85d8a0c5fde914861967e2a9a237
unknown <sebastien.rozange@MW773VKMUETGY6.dir.svc.accenture.com>
2015-04-02 19:08:02 +0200

    coco

--- src/main/java/org/betarss/controller/IndexController.java
+++ src/main/java/org/betarss/controller/IndexController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/*")
 public class IndexController {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
 	public String index() {
 		return "index.html";
 	}
