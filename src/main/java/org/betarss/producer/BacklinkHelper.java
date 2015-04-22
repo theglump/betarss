5fe4b5a3ebfa57cd94c5acfd14654bad387710db
unknown <sebastien.rozange@MW773VKMUETGY6.dir.svc.accenture.com>
2015-04-22 13:09:03 +0200

    Use baseurl

--- src/main/java/org/betarss/producer/BacklinkHelper.java
+++ src/main/java/org/betarss/producer/BacklinkHelper.java
@@ -1,5 +1,8 @@
 package org.betarss.producer;
 
+import java.io.IOException;
+
+import org.betarss.exception.BetarssException;
 import org.betarss.infrastructure.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,9 +12,14 @@ public class BacklinkHelper {
 
 	@Autowired
 	private ConfigurationService configurationService;
-	
+
 	public String create(String location) {
-		String applicationUrl = configurationService.getApplicationUrl();
+		String applicationUrl;
+		try {
+			applicationUrl = configurationService.getBaseUrl();
+		} catch (IOException e) {
+			throw new BetarssException(e);
+		}
 		return applicationUrl + (!applicationUrl.endsWith("/") ? "/" : "") + "torrent?location=" + location;
 	}
 }
